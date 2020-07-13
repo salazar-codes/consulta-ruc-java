ffa649f652ee74ba6b73b2bd69ca65e3ca3dbf37
jimmysn <jimmyr.salazar@gmail.com>
2020-07-13 13:41:10 -0500

    Refactorizando ConsultaRucServiceV2

--- consulta_ruc_sunat/src/main/java/com/jimmysalazar/consulta_ruc_sunat/ConsultaRucServiceV2.java
+++ consulta_ruc_sunat/src/main/java/com/jimmysalazar/consulta_ruc_sunat/ConsultaRucServiceV2.java
@@ -23,16 +23,30 @@ import net.sourceforge.tess4j.Tesseract;
 
 public class ConsultaRucServiceV2 {
 
+	final String PATH_CAPTCHA_FILES = "D:/captcha/";
+	final String PATH_TESSDATA_FILES = "D:/tessdata";
+	final String URL_WEB = "https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/frameCriterioBusqueda.jsp";
+	
+	private String currentIMG = "";
+	
 	WebClient webCLiente = new WebClient(); // Navegador
 	HtmlPage htmlPage = null; // Pagina a cargar
-
-	final String BASE_PATH = "D:/captcha/";
-	final String URL_WEB = "https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/frameCriterioBusqueda.jsp";
-	protected String currentIMG = "";
 
 	public void consultarRuc(String ruc) {
 		try {
 
+			loadingSearch();
+			String captchaGenerated = loadingReadCaptcha();
+			consultaServicio(ruc, captchaGenerated);
+			mostrarRespuestaSunat();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	private void loadingSearch() {
+		try {
 			System.out.println("********* Configurando cliente web *********");
 			webCLiente.getOptions().setJavaScriptEnabled(true);
 			webCLiente.getOptions().setCssEnabled(false);
@@ -41,47 +55,48 @@ public class ConsultaRucServiceV2 {
 			webCLiente.getOptions().setThrowExceptionOnScriptError(false);
 			webCLiente.getOptions().setThrowExceptionOnFailingStatusCode(false);
 			webCLiente.getOptions().setPrintContentOnFailingStatusCode(false);
-
+	
 			System.out.println("********* Cargando página *********");
 			htmlPage = webCLiente.getPage(URL_WEB);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
+	private String loadingReadCaptcha() {
+		try {
 			System.out.println("********* GENERANDO CAPTCHA*********");
-
 			HtmlImage image = htmlPage.<HtmlImage>getFirstByXPath("//img[@src='captcha?accion=image']");
 			ImageReader img = image.getImageReader();
 			BufferedImage buf = img.read(0);
 
 			// Generando la imagen
-			currentIMG = BASE_PATH + UUID.randomUUID() + ".jpg";
+			currentIMG = PATH_CAPTCHA_FILES + UUID.randomUUID() + ".jpg";
 			File outputfile = new File(currentIMG);
 			ImageIO.write(buf, "jpg", outputfile);
 
 			System.out.println("********* CAPTCHA GENERADO: " + currentIMG + " *********");
 
 			System.out.println("********* LEYENDO CAPTCHA*********");
-
 			Tesseract instance = getTesseract();
 			String captcha = instance.doOCR(outputfile);
 			captcha = captcha.replace(" ", "");
-			System.out.println(captcha);
-
-			System.out.println("********* CAPTCHA LEÍDO *********");
+			System.out.println("********* CAPTCHA LEÍDO: "+ captcha +" *********");
 
 			System.out.println("********* ELIMINANDO CAPTCHA*********");
 			if (outputfile.exists())
 				outputfile.delete();
 			System.out.println("********* CAPTCHA ELIMINADO *********");
-
-			System.out.println("********* CONSULTANDO A SUNAT *********");
-
-			consultaServicio(ruc, captcha);
-			mostrarRespuestaSunat();
-
+			
+			return captcha;
+			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
+			return null;
 		}
+		
 	}
-
+	
 	private void consultaServicio(String ruc, String codigoCaptcha) {
 		try {
 
@@ -156,11 +171,11 @@ public class ConsultaRucServiceV2 {
 
 	}
 
-	private static Tesseract getTesseract() {
+	private Tesseract getTesseract() {
 
 		Tesseract instance = new Tesseract();
 		// instance.setDatapath("/usr/local/Cellar/tesseract/4.0.0/share/tessdata");
-		instance.setDatapath("D:/tessdata");
+		instance.setDatapath(PATH_TESSDATA_FILES);
 		instance.setLanguage("eng");
 		instance.setHocr(false);
 		return instance;
